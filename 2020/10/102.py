4b169199a4b927c9127f2b63d60e3cd96839244a
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-10 16:30:12 +0100

    Day 10, part 2

--- 2020/10/102.py
+++ 2020/10/102.py
@@ -3,7 +3,6 @@ class Graph:
 
     def __init__(self, ints) -> None:
         super().__init__()
-        self.total_paths = 0
         self.ints = ints
         self.precalc = {}
 
@@ -14,13 +13,17 @@ class Graph:
                 next_idx.append(i)
         return next_idx
 
-    def count_paths(self, idx0):
+    def count_paths(self, idx0, curr_paths):
         if idx0 == len(self.ints) - 1:
-            self.total_paths += 1
+            return curr_paths + 1
 
+        if idx0 in self.precalc:
+            return self.precalc[idx0]
         for n in self.next_nodes(idx0):
-            # print(f'Child node ints[{n}]={self.ints[n]}')
-            self.count_paths(n)
+            child_paths = self.count_paths(n, curr_paths)
+            self.precalc[n] = child_paths
+            curr_paths += self.precalc[n]
+        return curr_paths
 
 
 def do_it(filename):
@@ -28,14 +31,12 @@ def do_it(filename):
         ints = sorted(list(map(int, f)))
         ints.insert(0, 0)
         ints.append(max(ints) + 3)
-    print(ints)
     graph = Graph(ints)
-    graph.count_paths(0)
-    return graph.total_paths
+    return graph.count_paths(0, 0)
 
 
 if __name__ == '__main__':
     output = do_it('input1.txt')
     print(f'Result: {output}')
 
-# Result: 1904
+# Result: 10578455953408
