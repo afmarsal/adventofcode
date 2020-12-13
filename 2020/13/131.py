a7ce4c21d83bf7e63426b3723c9753ebd731f308
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-13 11:18:42 +0100

    Day 13 part 1

--- 2020/13/131.py
+++ 2020/13/131.py
@@ -4,16 +4,15 @@ from itertools import count
 def do_it(filename):
     with open(filename) as f:
         v0 = int(f.readline().strip())
-        ids = f.readline().strip()
+        bus_ids = [int(c) for c in f.readline().strip().replace('x', '0').split(',')]
 
     freqs = {}
-    for id in ids.split(','):
-        if id == 'x':
-            continue
-        for i in count(0, int(id)):
-            if i >= v0:
-                freqs[int(id)] = i - v0
-                break
+    for bus_id in bus_ids:
+        if bus_id != 0:
+            for i in count(0, bus_id):
+                if i >= v0:
+                    freqs[bus_id] = i - v0
+                    break
 
     result = min(freqs, key=freqs.get)
     return result * freqs[result]
@@ -23,4 +22,4 @@ if __name__ == '__main__':
     output = do_it('input1.txt')
     print(f'Result: {output}')
 
-# Result: 2176
+# Result: 2305
