7bd6eaa87d9653640add3deeb89b3bf87d11f7f8
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-13 22:53:18 +0100

    Day 13 part 2

--- 2020/13/132.py
+++ 2020/13/132.py
@@ -1,33 +1,23 @@
-from itertools import count
-
-
 def do_it(filename):
     with open(filename) as f:
         v0 = int(f.readline().strip())
         bus_ids = [int(c) for c in f.readline().strip().replace('x', '0').split(',')]
 
-    freqs = [[bus_id, bus_id - i] for i, bus_id in enumerate(bus_ids) if bus_id > 0]
-    freqs.sort(key=lambda p: p[1], reverse=True)
+    # [id, offset]
+    freqs = [(bus_id, i) for i, bus_id in enumerate(bus_ids) if bus_id > 0]
     print(freqs)
 
-    while True:
-        freqs[0][1] += freqs[0][0]
-        next_val = freqs[0][1]
-        found = False
-        for freq in freqs[1:]:
-            while freq[1] < next_val:
-                freq[1] += freq[0]
-            found = freq[1] == next_val
-            if not found:
-                break
-        if found:
-            break
-
-    return next_val
+    step, result = freqs[0]
+    for bus_id, offset in freqs[1:]:
+        while (result + offset) % bus_id != 0:
+            result += step
+        step *= bus_id
+        # print(f'found! result: {result}, step: {step}')
+    return result
 
 
 if __name__ == '__main__':
-    output = do_it('input0.txt')
+    output = do_it('input1.txt')
     print(f'Result: {output}')
 
-# Result: 2305
+# Result: 552612234243498
