e6421acec5e2f5fe70147b1552d656fcee4e7899
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-17 09:42:52 +0100

    Day 17 fast

--- 2020/17/171.py
+++ 2020/17/171.py
@@ -1,6 +1,5 @@
 import itertools
 import operator
-import util
 
 
 def neighbours(coord, deltas):
@@ -9,32 +8,34 @@ def neighbours(coord, deltas):
 
 def calc_next(grid, deltas):
     next_coord = {n for coord in grid for n in neighbours(coord, deltas)}
-    next_grid = {}
+    next_grid = set()
     for coord in next_coord:
-        active = sum(grid.get(n, '.') == '#' for n in neighbours(coord, deltas))
-        if grid.get(coord, '.') == '#':
-            next_grid[coord] = '#' if 2 <= active <= 3 else '.'
-        else:
-            next_grid[coord] = '#' if active == 3 else '.'
+        active = sum(n in grid for n in neighbours(coord, deltas))
+        if coord in grid and 2 <= active <= 3:
+            next_grid.add(coord)
+        elif coord not in grid and active == 3:
+            next_grid.add(coord)
     return next_grid
 
 
 def do_it(grid, deltas):
     for i in range(6):
         grid = calc_next(grid, deltas)
-    return sum(c == '#' for c in grid.values())
+    return len(grid)
 
 
 if __name__ == '__main__':
-    filelines = util.lines('input1.txt')
+    with open('input1.txt') as f:
+        filelines = list(map(str.strip, f))
 
+    # grid is a set of active coordinates
     deltas3d = [c for c in itertools.product((0, 1, -1), repeat=3) if c != (0, 0, 0)]
-    grid3d = {(i, j, 0): c for i, line in enumerate(filelines) for j, c in enumerate(line)}
+    grid3d = {(i, j, 0) for i, line in enumerate(filelines) for j, c in enumerate(line) if c == '#'}
     output = do_it(grid3d, deltas3d)
     print(f'Part 1: {output}')
 
     deltas4d = [c for c in itertools.product((0, 1, -1), repeat=4) if c != (0, 0, 0, 0)]
-    grid4d = {(i, j, 0, 0): c for i, line in enumerate(filelines) for j, c in enumerate(line)}
+    grid4d = {(i, j, 0, 0) for i, line in enumerate(filelines) for j, c in enumerate(line) if c == '#'}
     output = do_it(grid4d, deltas4d)
     print(f'Part 2: {output}')
 
