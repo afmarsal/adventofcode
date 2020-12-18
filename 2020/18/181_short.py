8ed4b8dd1d34cb0e0ee37ab166fd6200e3c0d84b
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-18 20:20:54 +0100

    Day 18 short

--- 2020/18/181_short.py
+++ 2020/18/181_short.py
@@ -5,30 +5,23 @@ class Token:
 
     def __init__(self, value) -> None:
         self.val = int(value)
-
-    def __add__(self, other):
-        return Token(self.val + other.val)
+        Token.__add__ = Token.__pow__
+        Token.__mul__ = Token.__sub__
 
     def __sub__(self, other):
-        return Token(self.val * other.val)
-
-    def __mul__(self, other):
         return Token(self.val * other.val)
 
     def __pow__(self, other):
         return Token(self.val + other.val)
 
-    def __repr__(self):
-        return f'val: {self.val}'
 
-
-def replace(line, c1, c2):
-    return re.sub(r'(\d)', lambda m: f'Token({m[0]})', line.replace(c1, c2))
+def to_token(line):
+    return re.sub(r'(\d)', lambda m: f'Token({m[0]})', line)
 
 
 if __name__ == '__main__':
     with open('input0.txt') as f:
-        lines = list(map(str.strip, f))
+        lines = list(map(lambda line: to_token(line), f))
 
-    print('Part 1: ' + str(sum([eval(replace(line, '*', '-')).val for line in lines])))
-    print('Part 2: ' + str(sum([eval(replace(line, '+', '**')).val for line in lines])))
+    print('Part 1: ' + str(sum([eval(line.replace('*', '-')).val for line in lines])))
+    print('Part 2: ' + str(sum([eval(line.replace('+', '**')).val for line in lines])))
