bd168f3c9b90b04bfc5a81f3dd3868c953005926
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-24 15:06:28 +0100

    Day 24 part 1

---
+++ 2020/24/241.py
@@ -0,0 +1,46 @@
+from operator import add
+
+
+def parse_instructions(line):
+    idx = 0
+    instructions = []
+    while idx < len(line):
+        if line[idx] in {'e', 'w'}:
+            instructions.append(line[idx])
+            idx += 1
+        else:
+            instructions.append(line[idx] + line[idx + 1])
+            idx += 2
+    return instructions
+
+
+DELTAS = {
+    'e': lambda x: tuple(map(add, (2, 0), x)),
+    'w': lambda x: tuple(map(add, (-2, 0), x)),
+    'ne': lambda x: tuple(map(add, (1, 1), x)),
+    'se': lambda x: tuple(map(add, (1, -1), x)),
+    'nw': lambda x: tuple(map(add, (-1, 1), x)),
+    'sw': lambda x: tuple(map(add, (-1, -1), x)),
+}
+
+
+def do_it(lines):
+    grid = set()
+    for line in lines:
+        instructions = parse_instructions(line)
+        curr = (0, 0)
+        for op in instructions:
+            curr = DELTAS[op](curr)
+        if curr in grid:
+            grid.remove(curr)
+        else:
+            grid.add(curr)
+    return len(grid)
+
+
+if __name__ == '__main__':
+    with open('input1.txt') as f:
+        lines = list(map(str.strip, f))
+    print(f'Part 1: {do_it(lines)}')
+
+# Part 1: 263
