ff21adc0ce3a3be111e0543a83fd4b18763ae544
Albert Fernandez Marsal <albert.fernandez@king.com>
2020-12-27 17:36:37 +0100

    Day 2 short

--- 2020/01/010.py
+++ 2020/01/010.py
@@ -1,17 +1,8 @@
-def part1(nums):
-    return next(n * (2000 - n) for n in nums if (2000 - n) in nums)
-
-
-def part2(input_ints):
-    return next(
-        n1 * n2 * (2020 - n1 - n2) for n1 in input_ints for n2 in (input_ints - {n1}) if (2020 - n1 - n2) in input_ints)
-
-
 if __name__ == '__main__':
     with open('input01.txt') as f:
         nums = set(map(int, f))
-    print(f'Part 1: {part1(nums)}')
-    print(f'Part 2: {part2(nums)}')
+        print(f'Part 1: {next(n * (2000 - n) for n in nums if (2000 - n) in nums)}')
+        print(f'Part 2: {next(n1 * n2 * (2020 - n1 - n2) for n1 in nums for n2 in (nums - {n1}) if (2020 - n1 - n2) in nums)}')
 
 # Part 1: 556444
 # Part 2: 193598720
--- 2020/02/020.py
+++ 2020/02/020.py
@@ -1,13 +1,11 @@
-import util
+import re
 
-
-def do_it(filename):
-    parsed_input = util.read_regex_from_file(filename, r'^(\d+)-(\d+) (\w+): (\w+)$')
-    return sum((m[4][int(m[1]) - 1] == m[3]) != (m[4][int(m[2]) - 1] == m[3]) for m in parsed_input)
-
-
-if __name__ == '__main__':
-    output = do_it('input021.txt')
-    print(f'Result: {output}')
-
-# Result: 588
+with open("input021.txt") as f:
+    # Map lines in file to list of Match objects matching pattern
+    ml = list(map(lambda l: re.match(r'^(\d+)-(\d+) (\w+): (\w+)$', l), f))
+    # Method str.count returns the number of occurrences of a substring in a string. Then check
+    # if that count is between the allowed range. True evaluates to 1, so summing all gives the
+    # desired result
+    print("1: ", sum(int(m[1]) <= m[4].count(m[3]) <= int(m[2]) for m in ml))
+    # Sum the times it's True that ONLY 1 position match the condition (XOR)
+    print("2: ", sum((m[4][int(m[1]) - 1] == m[3]) != (m[4][int(m[2]) - 1] == m[3]) for m in ml))
