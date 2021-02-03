b856b08f4159314bee02a243dc7a872c392926fc
Albert Fernandez <afmarsal@hotmail.com>
2021-02-03 20:13:50 +0100

    2015 day 9 part 2

--- 2015/09/2015_09.py
+++ 2015/09/2015_09.py
@@ -10,14 +10,26 @@ def calc_dist(paths, route):
     return sum(paths[make_key(route[i], route[i + 1])] for i in range(len(route) - 1))
 
 
-def part1(lines):
+def parse_routes_and_paths(lines):
     paths = dict()
     nodes = set()
     for line in lines:
         node1, node2, dist = re.fullmatch(r'(\w+) to (\w+) = (\d+)', line).groups()
         nodes.update({node1, node2})
         paths[make_key(node1, node2)] = int(dist)
-    return min(calc_dist(paths, route) for route in it.permutations(nodes, len(nodes)))
+    return nodes, paths
+
+
+def calc_route(lines, f):
+    nodes, paths = parse_routes_and_paths(lines)
+    return f(calc_dist(paths, route) for route in it.permutations(nodes, len(nodes)))
+
+
+def part1(lines):
+    return calc_route(lines, min)
+
+def part2(lines):
+    return calc_route(lines, max)
 
 
 sample = '''London to Dublin = 464
@@ -30,4 +42,9 @@ lines = open('input.txt').read().splitlines()
 assert part1(lines) == 207
 print(f'# Part 1: {part1(lines)}')
 
+assert part2(sample.splitlines()) == 982
+assert part2(lines) == 804
+print(f'# Part 2: {part2(lines)}')
+
 # Part 1: 207
+# Part 2: 804
