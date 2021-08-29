4c40a34b42d3a49dcabf5731e7c74e187d970d76
Albert Fernandez <afmarsal@hotmail.com>
2021-08-29 14:02:02 +0200

    2015 day 17 part2

--- 2015/17/2015_17.py
+++ 2015/17/2015_17.py
@@ -14,7 +14,7 @@ def parse_input(lines):
 def solve(lines, top):
     capacities = parse_input(lines)
     print(f'Capacities: {capacities}')
-    combos = 0
+    combos = []
     stack = []
     idx = 0
     accum = 0
@@ -22,29 +22,35 @@ def solve(lines, top):
         partial = accum + capacities[idx]
         # print(f'Partial: {partial}, accum: {accum}, capacity: {capacities[idx]}, combos: {combos}, Stack: {[capacities[idx] for idx in stack]}')
         if partial == top:
-            combos += 1
+            combos.append(stack + [idx])
         elif partial < top:
             stack.append(idx)
             accum = partial
         idx += 1
         if idx >= len(capacities):
-            idx = stack.pop()
-            accum -= capacities[idx]
-            idx += 1
+            accum, idx = move_to_next_idx(accum, capacities, stack)
             if idx >= len(capacities):
                 if len(stack) == 0:
                     return combos
-                idx = stack.pop()
-                accum -= capacities[idx]
-                idx += 1
+                accum, idx = move_to_next_idx(accum, capacities, stack)
+
+
+def move_to_next_idx(accum, capacities, stack):
+    idx = stack.pop()
+    accum -= capacities[idx]
+    idx += 1
+    return accum, idx
 
 
 def part1(lines, top):
-    return solve(lines, top)
+    combos = solve(lines, top)
+    return len(combos)
 
 
 def part2(lines, top):
-    pass
+    combos = solve(lines, top)
+    lens = [len(c) for c in combos]
+    return Counter(lens)[min(lens)]
 
 
 class TestPart1(unittest.TestCase):
@@ -56,15 +62,19 @@ class TestPart1(unittest.TestCase):
     def test_input1(self):
         with open('input_part1.txt') as f:
             lines = f.read().splitlines()
-            # Not 5
             self.assertEqual(part1(lines, 150), 4372)
 
 
 class TestPart2(unittest.TestCase):
-    def test2(self):
+    def test_sample(self):
         with open('input0.txt') as f:
             lines = f.read().splitlines()
-            self.assertEqual(part2(lines), 241)
+            self.assertEqual(part2(lines, 25), 3)
+
+    def test_input1(self):
+        with open('input_part1.txt') as f:
+            lines = f.read().splitlines()
+            self.assertEqual(part2(lines, 150), 4)
 
 
 if __name__ == '__main__':
