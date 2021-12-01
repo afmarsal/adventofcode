d2a0127c2cd6d40e96d422cac63d7248cbccf6fc
Albert Fernandez <afmarsal@hotmail.com>
2021-12-01 10:07:25 +0100

    2021 day 2 Tests

--- 2021/01/01.py
+++ 2021/01/01.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def part1(nums):
     ini = nums[0]
@@ -7,21 +9,45 @@ def part1(nums):
             c += 1
         ini = l
 
-    print(f'Result {c}')
+    return c
+
 
 def part2(nums):
     ini = sum(nums[0:3])
     c = 0
     for j, __ in enumerate(nums[1:-2]):
-        i = j+1
-        curr = sum(nums[i:i+3])
+        i = j + 1
+        curr = sum(nums[i:i + 3])
         # print(f'ini: {ini}, c = {c}, i = {i}, curr = {curr}')
         if curr > ini:
             c += 1
         ini = curr
-    print(f'Result {c}')
+    return c
 
 
-nums = [int(l) for l in open('input.txt').read().splitlines()]
-# part1(nums)
-part2(nums)
+class TestPart1(unittest.TestCase):
+    def test10(self):
+        with open('input0.txt') as f:
+            nums = [int(l) for l in f.read().splitlines()]
+        self.assertEqual(part1(nums), 7)
+
+    def test1(self):
+        with open('input.txt') as f:
+            nums = [int(l) for l in f.read().splitlines()]
+        self.assertEqual(part1(nums), 1139)
+
+
+class TestPart2(unittest.TestCase):
+    def test20(self):
+        with open('input0.txt') as f:
+            nums = [int(l) for l in f.read().splitlines()]
+        self.assertEqual(part2(nums), 5)
+
+    def test21(self):
+        with open('input.txt') as f:
+            nums = [int(l) for l in f.read().splitlines()]
+        self.assertEqual(part2(nums), 1103)
+
+
+def get_nums():
+    return
