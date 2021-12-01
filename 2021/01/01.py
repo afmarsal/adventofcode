7d499133953320bbf59eaa0812e6e85eb7eee87c
Albert Fernandez <afmarsal@hotmail.com>
2021-12-01 10:02:04 +0100

    2021 day 2 part 2

--- 2021/01/01.py
+++ 2021/01/01.py
@@ -1,10 +1,27 @@
+
+def part1(nums):
+    ini = nums[0]
+    c = 0
+    for l in nums[1:]:
+        if l > ini:
+            c += 1
+        ini = l
+
+    print(f'Result {c}')
+
+def part2(nums):
+    ini = sum(nums[0:3])
+    c = 0
+    for j, __ in enumerate(nums[1:-2]):
+        i = j+1
+        curr = sum(nums[i:i+3])
+        # print(f'ini: {ini}, c = {c}, i = {i}, curr = {curr}')
+        if curr > ini:
+            c += 1
+        ini = curr
+    print(f'Result {c}')
+
+
 nums = [int(l) for l in open('input.txt').read().splitlines()]
-
-ini = nums[0]
-c = 0
-for l in nums[1:]:
-    if l > ini:
-        c += 1
-    ini = l
-
-print(f'Result {c}')
+# part1(nums)
+part2(nums)
