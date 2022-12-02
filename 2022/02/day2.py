f213384a97a46536c9fac4fd58fe0a2f6746ca7e
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-02 09:19:08 +0100

    2022 Day 2

--- 2022/02/day2.py
+++ 2022/02/day2.py
@@ -1,6 +1,6 @@
 import unittest
 
-RESULTS = {
+MAPPINGS = {
     'A': {'X': 3, 'Y': 6, 'Z': 0, 3: 'X', 6: 'Y', 0: 'Z'},
     'B': {'X': 0, 'Y': 3, 'Z': 6, 0: 'X', 3: 'Y', 6: 'Z'},
     'C': {'X': 6, 'Y': 0, 'Z': 3, 6: 'X', 0: 'Y', 3: 'Z'}
@@ -10,27 +10,14 @@ def read(filename):
     with open(filename) as f:
         return [tuple(n for n in l.split()) for l in f.read().splitlines()]
 
+def play(m):
+    return sum([MAPPINGS[g[0]][g[1]] + ord(g[1]) - ord('X') + 1 for g in m])
 
 def day1(filename):
-    # results = []
-    # for g in read(filename):
-    #     results.append(RESULTS[g[0]][g[1]] + ord(g[1]) - ord('X') + 1)
-    # print(results)
-    m = read(filename)
-    return play(m)
-
-
-def play(m):
-    return sum([RESULTS[g[0]][g[1]] + ord(g[1]) - ord('X') + 1 for g in m])
-
+    return play(read(filename))
 
 def day2(filename):
-    new_input = []
-    for g in read(filename):
-        expected_res = 3 * (ord(g[1]) - ord('X'))
-        my_move = list(RESULTS[g[0]].keys())[list(RESULTS[g[0]].values()).index(expected_res)]
-        new_input.append([g[0], my_move])
-    print(new_input)
+    new_input = [[g[0], (MAPPINGS[g[0]][(3 * (ord(g[1]) - ord('X')))])] for g in read(filename)]
     return play(new_input)
 
 
