35cab44058656b365a152737c46a596ee8696aac
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-03 18:31:50 +0100

    2022 Day 3 nicer?

--- 2022/03/day3.py
+++ 2022/03/day3.py
@@ -5,36 +5,31 @@ def read(filename):
     with open(filename) as f:
         return f.read().splitlines()
 
+
+first = lambda it: next(iter(it))
+priority = lambda c: ord(c) + 1 - (ord('a') if c.islower() else ord('A') - 26)
+
 def day1(filename):
-    res = 0
-    for l in read(filename):
-        common = set(l[:len(l) // 2]).intersection(l[len(l) // 2:])
-        c = next(iter(common))
-        prio = ord(c) + 1 - (ord('a') if c.islower() else ord('A') - 26)
-        # print('{} {} {}'.format(common, c, prio))
-        res += prio
-    return res
+    common_char = lambda l: first(set(l[:len(l) // 2]).intersection(l[len(l) // 2:]))
+    return sum([priority(common_char(line)) for line in read(filename)])
 
 def day2(filename):
     lines = read(filename)
     chunks = np.array_split(lines, len(lines) // 3)
-    res = 0
-    for chunk in chunks:
-        common = set.intersection(*[set(l) for l in chunk])
-        print(common)
-        c = next(iter(common))
-        prio = ord(c) + 1 - (ord('a') if c.islower() else ord('A') - 26)
-        res += prio
-    return res
+    common_char = lambda chunk: first(set.intersection(*[set(l) for l in chunk]))
+    return sum([priority(common_char(chunk)) for chunk in chunks])
 
 
 class TestPart1(unittest.TestCase):
     def test_sample(self):
-        f = 'sample.txt'
-        self.assertEqual(157, day1(f))
-        self.assertEqual(70, day2(f))
+        self.assertEqual(157, day1('sample.txt'))
 
     def test_input(self):
-        f = 'input.txt'
-        self.assertEqual(8072, day1(f))
-        self.assertEqual(2567, day2(f))
+        self.assertEqual(8072, day1('input.txt'))
+
+class TestPart2(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(70, day2('sample.txt'))
+
+    def test_input(self):
+        self.assertEqual(2567, day2('input.txt'))
