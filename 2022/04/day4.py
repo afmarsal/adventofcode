4aace9853f30010c8eda9891ce6024a2fbded845
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-04 11:44:10 +0100

    2022 day 4 without sets

--- 2022/04/day4.py
+++ 2022/04/day4.py
@@ -3,16 +3,17 @@ import itertools as it
 
 
 def read_ranges(filename):
-    to_set = lambda p: set(range(int(p[0]), int(p[1]) + 1))
     with open(filename) as f:
-        return [(to_set(r1.split('-')), to_set(r2.split('-')))
+        return [(list(map(int, r1.split('-'))), list(map(int, r2.split('-'))))
                 for r1, r2 in it.chain([line.split(",") for line in f.read().splitlines()])]
 
 def day1(filename):
-    return sum([int(rng1 >= rng2 or rng2 >= rng1) for rng1, rng2 in read_ranges(filename)])
+    return sum([int(p1[0] <= p2[0] <= p2[1] <= p1[1] or p2[0] <= p1[0] <= p1[1] <= p2[1])
+                for p1, p2 in read_ranges(filename)])
 
 def day2(filename):
-    return sum([int(not rng1.isdisjoint(rng2)) for rng1, rng2 in read_ranges(filename)])
+    return sum([int(max(p1[0], p2[0]) <= min(p1[1], p2[1]))
+                for p1, p2 in read_ranges(filename)])
 
 
 class TestPart1(unittest.TestCase):
