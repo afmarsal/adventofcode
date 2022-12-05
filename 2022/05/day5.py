679ae87f9db941eb2a369d6baa6e03cf3944546c
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-05 12:42:30 +0100

    2022 day 5 part 2

--- 2022/05/day5.py
+++ 2022/05/day5.py
@@ -3,17 +3,11 @@ import unittest
 import numpy as np
 
 
-def chunks(l, n):
-    """Yield n number of striped chunks from l."""
-    for i in range(0, n):
-        yield l[i::n]
-
-
 def read_crates(filename):
     columns = []
     with open(filename) as f:
-        for l in f.read().splitlines():
-            row = l[1::4]
+        for line in f.read().splitlines():
+            row = line[1::4]
             columns.append(np.array(list(row)))
         arr = np.array(columns).transpose()
         return [list(''.join(col).strip()) for col in arr.tolist()]
@@ -29,17 +23,19 @@ def read_moves(moves_file):
 def day1(crates_file, moves_file):
     crates = read_crates(crates_file)
     moves = read_moves(moves_file)
-    print(crates)
-    print(moves)
-    print()
     for move in moves:
         for i in range(int(move[0])):
             crates[move[2]-1].insert(0, crates[move[1]-1].pop(0))
     return ''.join([c[0] for c in crates])
 
 
-def day2(filename):
-    return -1
+def day2(crates_file, moves_file):
+    crates = read_crates(crates_file)
+    moves = read_moves(moves_file)
+    for move in moves:
+        crates[move[2] - 1][:0] = crates[move[1] - 1][:move[0]]
+        crates[move[1] - 1][0:move[0]] = []
+    return ''.join([c[0] for c in crates])
 
 
 class TestPart1(unittest.TestCase):
@@ -52,7 +48,7 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_sample(self):
-        self.assertEqual(4, day2('sample.txt'))
+        self.assertEqual('MCD', day2('sample_0.txt', 'sample_1.txt'))
 
     def test_input(self):
-        self.assertEqual(888, day2('input.txt'))
+        self.assertEqual('GGNPJBTTR', day2('input_0.txt', 'input_1.txt'))
