19e74b045e30bbd29ccdc7d6c328bbe8cf36eb05
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-05 12:55:45 +0100

    2022 day 5 simpler

--- 2022/05/day5.py
+++ 2022/05/day5.py
@@ -2,17 +2,18 @@ import re
 import unittest
 
 
-def read_crates(crates_file, moves_file):
+def read(crates_file, moves_file):
     with open(crates_file) as f:
-        columns = [list(line[1::4]) for line in f.read().splitlines()]
-        arr = list(map(list, zip(*columns)))  # transpose
-        crates = [[c for c in col if c != ' '] for col in arr]  # filter ' '
+        rows = [list(line[1::4]) for line in f.read().splitlines()]
+        columns = list(map(list, zip(*rows)))  # transpose
+        # This leaves top crate in position 0
+        crates = [[c for c in col if c != ' '] for col in columns]  # filter out ' '
     with open(moves_file) as f:
         moves = [list(map(int, re.findall(r"\d+", line))) for line in f.read().splitlines()]
     return crates, moves
 
 def day1(crates_file, moves_file):
-    crates, moves = read_crates(crates_file, moves_file)
+    crates, moves = read(crates_file, moves_file)
     for move in moves:
         for i in range(int(move[0])):
             crates[move[2]-1].insert(0, crates[move[1]-1].pop(0))
@@ -20,10 +21,10 @@ def day1(crates_file, moves_file):
 
 
 def day2(crates_file, moves_file):
-    crates, moves = read_crates(crates_file, moves_file)
+    crates, moves = read(crates_file, moves_file)
     for move in moves:
-        crates[move[2] - 1][:0] = crates[move[1] - 1][:move[0]]
-        crates[move[1] - 1][0:move[0]] = []
+        crates[move[2]-1][:0] = crates[move[1]-1][:move[0]]
+        crates[move[1]-1][0:move[0]] = []
     return ''.join([c[0] for c in crates])
 
 
