431934bf576c127819d4067a8be4af46ea905d35
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-08 18:25:43 +0100

    2022 Day 8 simpler

--- 2022/08/day8.py
+++ 2022/08/day8.py
@@ -11,34 +11,30 @@ def log(param):
     pass
 
 
-def vis(line, tree):
-    return all(tree > l for l in line)
-
-
-def visible(forest, y, x):
-    vis_left = vis(forest[y, :x], forest[y, x])
-    vis_right = vis(forest[y, x + 1:], forest[y, x])
-    vis_up = vis(forest[:y, x], forest[y, x])
-    vis_down = vis(forest[y + 1:, x], forest[y, x])
+def is_visible(forest, y, x):
+    vis_left  = all(forest[y, x] > t for t in forest[y, :x])
+    vis_right = all(forest[y, x] > t for t in forest[y, x+1:])
+    vis_up    = all(forest[y, x] > t for t in forest[:y, x])
+    vis_down  = all(forest[y, x] > t for t in forest[y+1:, x])
     return vis_left or vis_right or vis_up or vis_down
 
 
 def part1(filename):
     forest = np.array(read(filename))
     edges = (len(forest)-1) * 2 + (len(forest[0])-1) * 2
-    return edges + sum([int(visible(forest, y, x))
+    return edges + sum([int(is_visible(forest, y, x))
                         for y in range(1, len(forest) - 1)
                         for x in range(1, len(forest[y]) - 1)])
 
 def view(line, tree):
-    # Adding + 1 to count for the blocking tree. If no blocking, then it accounts for the edge
+    # Adding + 1 to count for the blocking tree. If no blocking, then it accounts for the edge.
     return len(list(it.takewhile(lambda v: v < tree, line))) + 1
 
 def scenic_score(forest, y, x):
-    dist_left = view(forest[y, x - 1:0:-1], forest[y, x])
-    dist_right = view(forest[y, x + 1:-1], forest[y, x])
-    dist_up = view(forest[y - 1:0:-1, x], forest[y, x])
-    dist_down = view(forest[y + 1:-1, x], forest[y, x])
+    dist_left  = view(forest[y, x-1:0:-1], forest[y, x])
+    dist_right = view(forest[y, x+1:-1], forest[y, x])
+    dist_up    = view(forest[y-1:0:-1, x], forest[y, x])
+    dist_down  = view(forest[y+1:-1, x], forest[y, x])
     return dist_left * dist_right * dist_up * dist_down
 
 
