43b525e4a2ce720f275d1622bc1139c338557941
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-10 09:40:32 +0100

    2022 Day 10 part 2

--- 2022/10/day10.py
+++ 2022/10/day10.py
@@ -46,7 +46,46 @@ def part1(filename):
     return sum(s for s in signal_strengths)
 
 def part2(filename):
-    return rope(filename, 10)
+    cycle = 1
+    rx = 1
+    signal_strengths = []
+    crt = [['.' for i in range(40)] for j in range(6)]
+    # for instr, delta in read(filename):
+    for instr in read(filename):
+        # log('cycle: {}, instr: {}, rx: {}'.format(cycle, instr, rx))
+        cycles_left = 1
+        cycling = False
+        while cycles_left > 0:
+            match instr:
+                case ['addx', _]:
+                    if not cycling:
+                        cycles_left = 2
+                        cycling = True
+                case ['noop']:
+                    pass
+                case _:
+                    raise Exception('Invalid instruction: {}'.format(instr))
+
+            crt_y, crt_x = divmod(cycle - 1, 40)
+            sprite_pos = {rx-1, rx, rx+1}
+            line = crt[crt_y][:]
+            line[rx-1:rx+2] = '###'
+            line[crt_x] = 'X'
+            log('cycle: {}, line: {}'.format(cycle, ''.join(line)))
+            if crt_x in sprite_pos:
+                crt[crt_y][crt_x] = '◼'
+                log('cycle: {}, lit {},{}'.format(cycle+1, crt_y, crt_x))
+            cycles_left -= 1
+            cycle += 1
+        match instr:
+            case ['addx', delta]:
+                rx += int(delta)
+
+    for line in crt:
+        print(''.join(line))
+    return sum(s for s in signal_strengths)
+
+
 
 class TestPart1(unittest.TestCase):
     def test_sample(self):
