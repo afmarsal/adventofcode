a3fe1f8d5fb77caab41a176e4868aeeac25b28de
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-11 08:39:46 +0100

    2022 Day 11 part 2

--- 2022/11/day11.py
+++ 2022/11/day11.py
@@ -1,5 +1,6 @@
 import unittest
 import re
+import numpy
 
 def read(filename):
     monkeys = []
@@ -34,9 +35,26 @@ def part1(filename):
     inspections.sort()
     return inspections[-1] * inspections[-2]
 
-def part2(filename):
-    return -1
 
+def part2(filename):
+    monkeys = read(filename)
+    mcm = numpy.prod([monkey['div'] for monkey in monkeys])
+    for round in range(10000):
+        for monkey in monkeys:
+            for item in monkey['items']:
+                monkey['inspected'] += 1
+                op1 = int(item) if monkey['op'][0] == 'old' else int(monkey['op'][0])
+                op2 = int(item) if monkey['op'][2] == 'old' else int(monkey['op'][2])
+                new_level = op1 * op2 if monkey['op'][1] == '*' else op1 + op2
+                new_level = new_level % mcm
+                if new_level % monkey['div'] == 0:
+                    monkeys[monkey['true']]['items'].append(new_level)
+                else:
+                    monkeys[monkey['false']]['items'].append(new_level)
+            monkey['items'] = []
+    inspections = [monkey['inspected'] for monkey in monkeys]
+    inspections.sort()
+    return inspections[-1] * inspections[-2]
 
 class TestPart1(unittest.TestCase):
     def test_sample(self):
@@ -48,7 +66,7 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_sample(self):
-        self.assertEqual(-1, part2('sample.txt'))
+        self.assertEqual(2713310158, part2('sample.txt'))
 
     def test_input(self):
-        self.assertEqual(-2, part2('input.txt'))
+        self.assertEqual(14561971968, part2('input.txt'))
