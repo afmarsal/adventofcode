e9b54e56f1264a3ffcc4d6a541646eb13a4aed6a
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-18 00:35:19 +0100

    2022 Day 17 part 2 WIP broken input part 1

--- 2022/17/day17.py
+++ 2022/17/day17.py
@@ -12,18 +12,13 @@ def log_nolf(param):
     log(param, end='')
 
 
-def build_chamber():
+def build_pieces():
     piece1 = {(0, 0), (1, 0), (2, 0), (3, 0)}
     piece2 = {(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)}
     piece3 = {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)}
     piece4 = {(0, 0), (0, 1), (0, 2), (0, 3)}
     piece5 = {(0, 0), (0, 1), (1, 0), (1, 1)}
-    pieces = [piece1, piece2, piece3, piece4, piece5]
-
-    # wall_left = {(0, y) for y in range(4000)}
-    # wall_right = {(8, y) for y in range(4000)}
-    chamber = {(x, 0) for x in range(9)}
-    return chamber, pieces
+    return [piece1, piece2, piece3, piece4, piece5]
 
 
 def move_to(piece, x_offset, max_y):
@@ -80,25 +75,56 @@ def settle_piece(jets, jet_idx, piece, base):
         # log_chamber(chamber, piece)
     return jet_idx, chamber
 
-def part1(filename):
+def calc_height(filename, rocks):
     jets = read(filename)
-    chamber, pieces = build_chamber()
+    pieces = build_pieces()
 
-    rocks = 2022
+    cycle_start = 16
+    rocks_in_cycle = 35
+    # After cycle_start (16) iterations, there's a cycle of rocks_in_cycle (35)
+
+    # Start the first group of rocks
     jet_idx = 0
     chamber = set()
-    for i in range(rocks):
-        piece = pieces[i % len(pieces)]
+    for rock in range(cycle_start):
+        piece = pieces[rock % len(pieces)]
         jet_idx, chamber = settle_piece(jets, jet_idx, piece, chamber)
-        log(f'rock: {i}, jet: {jet_idx % len(jets)}')
+    next_rock = cycle_start
+    max_y = max(y for x, y in chamber)
+    # log_chamber(chamber)
+    first_height = max_y
+
+    # Calculate the delta height for a cycle
+    jet_idx2 = jet_idx
+    log(f'rock: {next_rock}, jet: {jet_idx2}')
+    for rock in range(next_rock, next_rock + rocks_in_cycle):
+        piece = pieces[rock % len(pieces)]
+        jet_idx2, chamber = settle_piece(jets, jet_idx2, piece, chamber)
+        log(f'rock: {rock}, jet: {jet_idx2}')
+    max_y = max(y for x, y in chamber)
+    height_delta = max_y - first_height
+
+    # Now put on top the last "batch" of rocks
+    num_cycles, remainder = divmod(rocks - cycle_start, rocks_in_cycle)
+    jet_idx3 = jet_idx2
+    next_rock = next_rock + rocks_in_cycle
+    for rock in range(next_rock, next_rock + remainder):
+        piece = pieces[rock % len(pieces)]
+        jet_idx3, chamber = settle_piece(jets, jet_idx3, piece, chamber)
+        log(f'rock: {rock}, jet: {jet_idx2}')
+    max_y = max(y for x, y in chamber)
+    last_height = max_y - (first_height + height_delta)
 
     log_chamber(chamber)
-    return max(y for x, y in chamber)
+
+    return first_height + height_delta * num_cycles + last_height
+
+def part1(filename):
+    return calc_height(filename, 2022)
+
 
 def part2(filename):
-    jets = read(filename)
-
-    return -1
+    return calc_height(filename, 1000000000000)
 
 class TestPart1(unittest.TestCase):
     def test_sample(self):
@@ -112,4 +138,5 @@ class TestPart2(unittest.TestCase):
         self.assertEqual(1514285714288, part2('sample.txt'))
 
     def test_input(self):
+        # 1571428571425 too high
         self.assertEqual(-2, part2('input.txt'))
