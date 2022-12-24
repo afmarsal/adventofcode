23d3c83951be725b325734033b2e78c1dfa7f14d
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-24 10:23:24 +0100

    2022 Day 23 part 1

---
+++ 2022/23/day23.py
@@ -0,0 +1,127 @@
+import dataclasses
+import unittest
+from collections import OrderedDict
+
+
+def read(filename):
+    with open(filename) as f:
+        pairs = [((x, y), DIRECTIONS.copy()) for y, line in enumerate(f.read().splitlines()) for x, c in enumerate(line) if c == '#']
+        return LastUpdatedOrderedDict((p[0], p[1]) for p in pairs)
+
+OFFSETS = {
+    'N': [(-1, -1), (0, -1), (1, -1)],
+    'S': [(-1, 1),  (0, 1),  (1, 1)],
+    'W': [(-1, -1), (-1, 0), (-1, 1)],
+    'E': [(1, -1),  (1, 0),  (1, 1)],
+}
+ALL_OFFSETS = {o for k, v in OFFSETS.items() for o in v}
+
+DIRECTIONS = ['N', 'S', 'W', 'E']
+
+def log(param='', end='\n'):
+    print(param, end=end)
+    pass
+
+def log_nolf(param):
+    log(param, end='')
+
+
+class LastUpdatedOrderedDict(OrderedDict):
+    'Store items in the order the keys were last added'
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.move_to_end(key)
+
+def print_map(elves):
+    min_x = min(t[0] for t in elves) - 2
+    min_y = min(t[1] for t in elves) - 2
+    max_x = max(t[0] for t in elves) + 2
+    max_y = max(t[1] for t in elves) + 2
+
+    for y in range(min_y, max_y+1):
+        for x in range(min_x, max_x + 1):
+            log_nolf('#' if (x,y) in elves else '.')
+        log()
+
+def add(t1, t2):
+    return t1[0] + t2[0], t1[1] + t2[1]
+
+def part1(filename):
+    elves = read(filename)
+    print_map(elves.keys())
+    for i in range(10):
+        next_elves = LastUpdatedOrderedDict()
+        curr_positions = elves.keys()
+        # for each position, which elves end up there
+        next_positions = {}
+        for elf_pos, elf_directions in elves.items():
+            # Elf not surrounded
+            if all(add(elf_pos, o) not in curr_positions for o in ALL_OFFSETS):
+                log(f'Elf at {elf_pos}: {elf_directions} not moving because its isolated')
+                next_positions[elf_pos] = [(elf_pos, False)]
+                continue
+            for d in elf_directions:
+                log(f'Trying to move elf at {elf_pos} {d}')
+                for o in OFFSETS[d]:
+                    new_pos = add(elf_pos, o)
+                    if new_pos in curr_positions:
+                        log(f'Elf at {elf_pos} can\'t move {o}')
+                        break
+                else:
+                    new_pos = add(elf_pos, OFFSETS[d][1])  # Final position is the "middle" offset
+                    if new_pos not in next_positions:
+                        next_positions[new_pos] = []
+                    log(f'Elf at {elf_pos} will move to {new_pos}')
+                    next_positions[new_pos].append((elf_pos, True))
+                    break
+            else:
+                # Could not find a new pos to move
+                log(f'Elf at {elf_pos} could not move in any direction')
+                next_positions[elf_pos] = [(elf_pos, True)]
+
+        for next_pos, data in next_positions.items():
+            if len(data) == 1:
+                original_pos, has_to_move = data[0]
+                if not has_to_move:
+                    if next_pos != original_pos:
+                        raise Exception(f'Positions {original_pos} and {next_pos} don\'t match!')
+                    next_elves[next_pos] = elves[original_pos][1:] + [elves[original_pos][0]]
+                    log(f'Elf at {original_pos} stays in same pos. Keeping directions: {next_elves[next_pos]}')
+                else:
+                    next_elves[next_pos] = elves[original_pos][1:] + [elves[original_pos][0]]
+                    log(f'Moved elf at {original_pos} to {next_pos}. New directions: {next_elves[next_pos]}')
+            else:
+                log(f'More than one elf at {next_pos}: can\'t move')
+                for original_pos, _ in data:
+                    next_elves[original_pos] = elves[original_pos][1:] + [elves[original_pos][0]]
+                    log(f'Updating directions for elf at {original_pos}: {next_elves[original_pos]}')
+
+        elves = next_elves
+        log(f'\nEnd of round {i+1}')
+        print_map(elves)
+
+    min_x = min(t[0] for t in elves)
+    min_y = min(t[1] for t in elves)
+    max_x = max(t[0] for t in elves)
+    max_y = max(t[1] for t in elves)
+    print_map(elves)
+    return (max_y - min_y + 1) * (max_x - min_x + 1) - len(elves)
+
+def part2(filename):
+    return -1
+
+
+class TestPart1(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(110, part1('sample.txt'))
+
+    def test_input(self):
+        self.assertEqual(3917, part1('input.txt'))
+
+class TestPart2(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(20, part2('sample.txt'))
+
+    def test_input(self):
+        self.assertEqual(-2, part2('input.txt'))
