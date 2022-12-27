68fd81e55431cb79778f769a60a0604f1076f6ba
Albert Fernandez Marsal <albert.fernandez@king.com>
2022-12-27 09:57:38 +0100

    2022 Day 16 part 2

--- 2022/16/day16.py
+++ 2022/16/day16.py
@@ -27,8 +27,6 @@ def log(param='', end='\n'):
 def log_nolf(param):
     log(param, end='')
 
-TOTAL_MINUTES = 30
-
 def build_graph(scan):
     graph = nx.DiGraph()
     for node in scan:
@@ -37,41 +35,85 @@ def build_graph(scan):
             graph.add_edge(node, child)
     return graph
 
-
 @functools.cache
 def best_path(graph, valve1, valve2):
     return len(nx.shortest_path(graph, valve1, valve2)) - 1
 
-def best_path_cost(graph, remaining_minutes, curr_node, remaning_nodes, curr_path):
+def best_path_cost1(graph, remaining_minutes, curr_node, remaning_nodes, curr_path):
     if len(remaning_nodes) == 0:
         return 0
-    else:
-        costs = []
-        for node in remaning_nodes:
-            c = best_path(graph, curr_node, node)
-            next_remaining_minutes = remaining_minutes - (c + 1)
-            if next_remaining_minutes <= 0:
-                continue
-            release = next_remaining_minutes * graph.nodes[node]['rate']
-            next_remaning_nodes = tuple(set(remaning_nodes) - {node})
-            next_path = curr_path + [node]
-            log(f'Cost from {curr_node} to {node}. Release: {release} = {remaining_minutes} * '
-                f'{graph.nodes[node]["rate"]}. Recursing: {node} -> {next_remaning_nodes}')
-            cost = release + best_path_cost(graph, next_remaining_minutes, node, next_remaning_nodes, next_path)
-            costs.append((remaning_nodes, cost))
-            log(f'Cost from {curr_node} to {node}. Release: {release}, cost: {cost}')
-        if len(costs) == 0:
-            return 0
-        return max(c for p, c in costs)
+    costs = []
+    for node in remaning_nodes:
+        c = best_path(graph, curr_node, node)
+        next_remaining_minutes = remaining_minutes - (c + 1)
+        if next_remaining_minutes <= 0:
+            continue
+        release = next_remaining_minutes * graph.nodes[node]['rate']
+        next_remaning_nodes = tuple(set(remaning_nodes) - {node})
+        next_path = curr_path + [node]
+        log(f'Cost from {curr_node} to {node}. Release: {release} = {remaining_minutes} * '
+            f'{graph.nodes[node]["rate"]}. Recursing: {node} -> {next_remaning_nodes}')
+        cost = release + best_path_cost1(graph, next_remaining_minutes, node, next_remaning_nodes, next_path)
+        costs.append((remaning_nodes, cost))
+        log(f'Cost from {curr_node} to {node}. Release: {release}, cost: {cost}')
+    if len(costs) == 0:
+        return 0
+    return max(c for p, c in costs)
 
 def part1(filename):
     scan = read(filename)
     graph = build_graph(scan)
     valves = {k for k, v in scan.items() if v.rate > 0}
-    return best_path_cost(graph, TOTAL_MINUTES, 'AA', tuple(valves), ['AA'])
+    return best_path_cost1(graph, 30, 'AA', tuple(valves), ['AA'])
+
+def best_path_cost2(graph, min1, min2, curr_node1, curr_node2, remaning_nodes, curr_path1, curr_path2):
+    if len(remaning_nodes) == 0:
+        return 0
+    costs = []
+    if min1 >= min2:
+        remaining_minutes = min1
+        curr_node = curr_node1
+        advance1 = True
+    else:
+        remaining_minutes = min2
+        curr_node = curr_node2
+        advance1 = False
+    log(f'Advancing1: {advance1}. min1: {min1}, min2: {min2}, node1: {curr_node1}, node2: {curr_node2}')
+    for node in remaning_nodes:
+        c = best_path(graph, curr_node, node)
+        next_remaining_minutes = remaining_minutes - (c + 1)
+        if next_remaining_minutes <= 0:
+            continue
+        if advance1:
+            next_min1 = next_remaining_minutes
+            next_min2 = min2
+            next_node1 = node
+            next_node2 = curr_node2
+            next_path1 = curr_path1 + [node]
+            next_path2 = curr_path2
+        else:
+            next_min1 = min1
+            next_min2 = next_remaining_minutes
+            next_node1 = curr_node1
+            next_node2 = node
+            next_path1 = curr_path1
+            next_path2 = curr_path2 + [node]
+        release = next_remaining_minutes * graph.nodes[node]['rate']
+        next_remaning_nodes = tuple(set(remaning_nodes) - {node})
+        log(f'Cost from {curr_node} to {node}. Release: {release} = {remaining_minutes} * '
+            f'{graph.nodes[node]["rate"]}. Recursing: {node} -> {next_remaning_nodes}')
+        cost = release + best_path_cost2(graph, next_min1, next_min2, next_node1, next_node2, next_remaning_nodes, next_path1, next_path2)
+        costs.append((remaning_nodes, cost))
+        log(f'Cost from {curr_node} to {node}. Release: {release}, cost: {cost}')
+    if len(costs) == 0:
+        return 0
+    return max(c for p, c in costs)
 
 def part2(filename):
-    return -1
+    scan = read(filename)
+    graph = build_graph(scan)
+    valves = {k for k, v in scan.items() if v.rate > 0}
+    return best_path_cost2(graph, 26, 26, 'AA', 'AA', tuple(valves), ['AA'], ['AA'])
 
 
 class TestPart1(unittest.TestCase):
@@ -83,7 +125,7 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_sample(self):
-        self.assertEqual(-2, part2('sample.txt'))
+        self.assertEqual(1707, part2('sample.txt'))
 
     def test_input(self):
-        self.assertEqual(-2, part2('input.txt'))
+        self.assertEqual(2602, part2('input.txt'))
