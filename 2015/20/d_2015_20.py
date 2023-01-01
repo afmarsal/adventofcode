af40c0ef7e689eb3d2afdda4cbe79767479acf1e
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-01-01 21:39:00 +0100

    2015 day 20 part 2

--- 2015/20/d_2015_20.py
+++ 2015/20/d_2015_20.py
@@ -22,8 +22,13 @@ def part1(num):
     return house
 
 
-def part2(filename):
-    return -1
+def part2(num):
+    s, house = 0, 0
+    while s < num:
+        house += 1
+        s = sum(d * 11 for d in divisors(house) if d * 50 >= house)
+        # log(f'House: {house}, sum: {s}')
+    return house
 
 
 class TestPart1(unittest.TestCase):
@@ -40,7 +45,14 @@ class TestPart1(unittest.TestCase):
 
 class TestPart2(unittest.TestCase):
     def test_sample(self):
-        self.assertEqual(-2, part2('sample.txt'))
+        # self.assertEqual(1, part2(11))
+        # self.assertEqual(2, part2(30))
+        # self.assertEqual(3, part2(40))
+        # self.assertEqual(4, part2(70))
+        # self.assertEqual(6, part2(120))
+        self.assertEqual(8, part2(150))
 
     def test_input(self):
-        self.assertEqual(-2, part2('input.txt'))
+        # 942480 too high
+        # 2040000 too high
+        self.assertEqual(-2, part2(34000000))
