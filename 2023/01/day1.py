101f2727ffe44f226bef7dc95f9ebed624fa8a95
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-01 10:05:57 +0100

    2023 day 1 part 2

--- 2023/01/day1.py
+++ 2023/01/day1.py
@@ -17,23 +17,52 @@ def part1(filename):
     for line in lines:
         only_digits = [d for d in line if d.isdigit()]
         line_val = int(only_digits[0]) * 10 + int(only_digits[-1])
-        print(line_val)
         res += line_val
     return res
 
+repl = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
 
 def part2(filename):
-    print(get_lines(filename))
-    return sum(sorted(get_lines(filename))[-3:])
+    lines = get_lines(filename)
+    res = 0
+    for line in lines:
+        p = int(find_first_digit(line, False)) * 10
+        p += int(find_first_digit(line[::-1], True))
+        res += p
+    return res
+
+
+def find_first_digit(line, reversed):
+    for i in range(len(line)):
+        if line[i].isdigit():
+            return line[i]
+        for old, new in repl.items():
+            old = old[::-1] if reversed else old
+            if line[i:].startswith(old):
+                return new
+    return -1
 
 
 class TestPart1(unittest.TestCase):
-    def test_sample(self):
+    def test_part1(self):
         f = 'sample.txt'
         self.assertEqual(142, part1(f))
-        # self.assertEqual(45000, part2(f))
-
-    def test_input(self):
         f = 'input.txt'
         self.assertEqual(53080, part1(f))
-        # self.assertEqual(201524, part2(f))
+
+    def test_input(self):
+        f = 'sample2.txt'
+        self.assertEqual(281, part2(f))
+        f = 'input.txt'
+        self.assertEqual(53268, part2(f))
+        # self.assertEqual(45000, part2(f))
