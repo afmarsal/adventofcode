376c6d828197860a435b792fb0ba362cc2156ffa
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-02 17:09:56 +0100

    2023 day 1 short

--- 2023/01/day1.py
+++ 2023/01/day1.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 
 
@@ -7,12 +8,16 @@ def get_lines(filename):
 
 
 def part1(filename):
-    lines = get_lines(filename)
-    res = 0
-    for line in lines:
-        only_digits = [d for d in line if d.isdigit()]
-        res += int(only_digits[0] + only_digits[-1])
-    return res
+    with open(filename) as f:
+        text = f.read()
+    return sum(int(nt[0] + nt[-1]) for nt in re.sub(r"[A-z]", "", text).split("\n"))
+    # lines = get_lines(filename)
+    # res = 0
+    # for line in lines:
+    #     re.sub(r'[A-z]', '', line)
+    #     only_digits = [d for d in line if d.isdigit()]
+    #     res += int(only_digits[0] + only_digits[-1])
+    # return res
 
 
 repl = {
@@ -33,17 +38,18 @@ def part2(filename):
     res = 0
     for line in lines:
         first = find_first_digit(line, False)
-        last = find_first_digit(line[::-1], True)
+        last = find_first_digit(line, True)
         res += int(first + last)
     return res
 
 
-def find_first_digit(line, reversed):
+def find_first_digit(line, reverse):
+    line = line[::-1] if reverse else line
     for i in range(len(line)):
         if line[i].isdigit():
             return line[i]
         for old, new in repl.items():
-            old = old[::-1] if reversed else old
+            old = old[::-1] if reverse else old
             if line[i:].startswith(old):
                 return new
     return -1
