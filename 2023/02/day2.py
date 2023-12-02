7f217e66b2b9c1a7141b220e0d4b16795f214b80
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-02 09:05:00 +0100

    2023 day 2 part2

--- 2023/02/day2.py
+++ 2023/02/day2.py
@@ -1,6 +1,7 @@
 import re
 import unittest
-
+import functools
+import operator
 
 def get_lines(filename):
     with open(filename) as f:
@@ -34,19 +35,30 @@ def part1(filename):
 
 
 def part2(filename):
-
+    lines = get_lines(filename)
+    res = 0
+    for line in lines:
+        bags_part = line.split(':')[1]
+        maxes = {'red': 0, 'green': 0, 'blue': 0}
+        for hand in bags_part.split(';'):
+            for draw in hand.split(','):
+                n, c = draw.strip().split(' ')
+                maxes[c] = max(maxes[c], int(n))
+        print(maxes)
+        res += functools.reduce(operator.mul, maxes.values())
     return res
+
 
 class TestAll(unittest.TestCase):
     def test_part1(self):
         f = 'sample.txt'
         self.assertEqual(8, part1(f))
         f = 'input.txt'
-        self.assertEqual(53080, part1(f))
+        self.assertEqual(3059, part1(f))
 
     def test_part2(self):
-        f = 'sample2.txt'
-        self.assertEqual(281, part2(f))
+        f = 'sample.txt'
+        self.assertEqual(2286, part2(f))
         f = 'input.txt'
-        self.assertEqual(53268, part2(f))
+        self.assertEqual(65371, part2(f))
 
