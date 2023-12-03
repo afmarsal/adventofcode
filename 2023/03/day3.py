71cd25b81f88ddb9c6ab51175c4b373d2c4f9331
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-03 08:08:51 +0100

    2023 day 3 part 2

--- 2023/03/day3.py
+++ 2023/03/day3.py
@@ -2,6 +2,8 @@ import re
 import unittest
 import functools
 import operator as op
+from collections import defaultdict
+
 
 def get_lines(filename):
     with open(filename) as f:
@@ -50,8 +52,42 @@ def part1(filename):
     return sum(numbers)
 
 
+def find_star(j, i, lines):
+    for a in around:
+        coord = tuple(map(op.add, a, (j, i)))
+        if 0 <= coord[0] < len(lines) and 0 <= coord[1] < len(lines[i]):
+            if lines[coord[0]][coord[1]] == '*':
+                return coord
+    return None
+
+
 def part2(filename):
-    return res
+    lines = get_lines(filename)
+    numbers_and_stars = defaultdict(list)
+    for j, line in enumerate(lines):
+        cur_number = 0
+        cur_star = None
+        for i, c in enumerate(line):
+            if c.isdigit():
+                cur_number = cur_number * 10 + int(c)
+                if not cur_star:
+                    coord = find_star(j, i, lines)
+                    if coord:
+                        print(f'Found star for {j},{i}. Cur num: {cur_number}')
+                        cur_star = coord
+            else:
+                if cur_star and cur_number > 0:
+                    print(f'Adding number for star: {cur_star} -> {cur_number}')
+                    numbers_and_stars[cur_star].append(cur_number)
+                cur_number = 0
+                cur_star = None
+
+        if cur_star and cur_number > 0:
+            print(f'Adding number for star: {cur_star} -> {cur_number}')
+            numbers_and_stars[cur_star].append(cur_number)
+
+    print(numbers_and_stars)
+    return sum([v[0]*v[1] for v in numbers_and_stars.values() if len(v) == 2])
 
 
 class TestAll(unittest.TestCase):
@@ -63,7 +99,7 @@ class TestAll(unittest.TestCase):
 
     def test_part2(self):
         f = 'sample.txt'
-        self.assertEqual(2286, part2(f))
+        self.assertEqual(467835, part2(f))
         f = 'input.txt'
-        self.assertEqual(65371, part2(f))
+        self.assertEqual(89471771, part2(f))
 
