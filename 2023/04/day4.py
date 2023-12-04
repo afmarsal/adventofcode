c58df6e013d9ab4ec9c964872f61a07668a45892
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-04 10:07:49 +0100

    2023 day 4 part 2

--- 2023/04/day4.py
+++ 2023/04/day4.py
@@ -1,3 +1,4 @@
+import operator
 import re
 import unittest
 import functools
@@ -28,7 +29,19 @@ def part1(filename):
 
 def part2(filename):
     lines = get_lines(filename)
-
+    res = 1
+    total_cards = [1] * len(lines)
+    for i, line in enumerate(lines):
+        cards = line.split(':')[1]
+        winners, numbers = cards.split('|')
+        winners = {s.strip() for s in winners.split()}
+        numbers = {s.strip() for s in numbers.split()}
+        combo = len(winners.intersection(numbers))
+        print(f'i: {i+1}, w: {winners}, n: {numbers}, c:{combo}')
+        for j in range(i+1, i+1+combo):
+            total_cards[j] += total_cards[i]
+        print(f'tc: {total_cards}')
+    return sum(total_cards)
 
 
 class TestAll(unittest.TestCase):
@@ -40,7 +53,7 @@ class TestAll(unittest.TestCase):
 
     def test_part2(self):
         f = 'sample.txt'
-        self.assertEqual(467835, part2(f))
+        self.assertEqual(30, part2(f))
         f = 'input.txt'
-        self.assertEqual(89471771, part2(f))
+        self.assertEqual(5704953, part2(f))
 
