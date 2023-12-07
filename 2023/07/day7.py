ffbf95bf29b4c3f169aa53b9d75b85e1bbf7f0d5
Albert Fernandez Marsal <albert.fernandez@king.com>
2023-12-07 08:08:23 +0100

    2023 day 7 part 1

--- 2023/07/day7.py
+++ 2023/07/day7.py
@@ -24,16 +24,18 @@ def log_nolf(param):
 
 def group_hand(hand):
     dict1 = Counter(hand)
-    res = defaultdict(list)
+    res = defaultdict(set)
     for key, value in dict1.items():
-        res[value].append(key)
+        res[value].add(key)
     return res
 
 
 def hand_cards_values1(hand):
     card_values = {c: i for i, c in enumerate(reversed("A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")))}
-    l = [pow(100, len(hand)-i-1)*card_values[c] for i, c in enumerate(hand)]
-    log(f'{hand}: {l}')
+    return sum(pow(100, len(hand)-i)*card_values[c] for i, c in enumerate(hand))
+
+def hand_cards_values2(hand):
+    card_values = {c: i for i, c in enumerate(reversed("A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".split(", ")))}
     return sum(pow(100, len(hand)-i)*card_values[c] for i, c in enumerate(hand))
 
 
@@ -56,6 +58,60 @@ def compute_hand1(p):
     log(f'Evaluated {hand}: {res}')
     return res
 
+def compute_hand2(p):
+    hand, grouped_hand, bid = p
+    if 5 in grouped_hand:
+        type = '5k'
+    elif 4 in grouped_hand:
+        if 'J' in hand:
+            type = '5k'
+        else:
+            type = '4k'
+    elif 3 in grouped_hand and 2 in grouped_hand:
+        if 'J' in hand:
+            type = '5k'
+        else:
+            type = 'fh'
+    elif 3 in grouped_hand:
+        if 'J' in grouped_hand[3] or 'J' in hand:
+            # 3J or 1J
+            type = '4k'
+        else:
+            type = '3k'
+    elif 2 in grouped_hand and len(grouped_hand[2]) == 2:
+        if 'J' in grouped_hand[2]:
+            type = '4k'
+        elif 'J' in hand:
+            type = 'fh'
+        else:
+            type = '2p'
+    elif 2 in grouped_hand:
+        if 'J' in grouped_hand[2] or 'J' in hand:
+            type = '3k'
+        else:
+            type = '2k'
+    else:
+        if 'J' in hand:
+            type = '2k'
+        else:
+            type = '1k'
+    if type == '5k':
+        res = 7 * TYPE_WEIGHT + hand_cards_values2(hand)
+    elif type == '4k':
+        res = 6 * TYPE_WEIGHT + hand_cards_values2(hand)
+    elif type == 'fh':
+        res = 5 * TYPE_WEIGHT + hand_cards_values2(hand)
+    elif type == '3k':
+        res = 4 * TYPE_WEIGHT + hand_cards_values2(hand)
+    elif type == '2p':
+        res = 3 * TYPE_WEIGHT + hand_cards_values2(hand)
+    elif type == '2k':
+        res = 2 * TYPE_WEIGHT + hand_cards_values2(hand)
+    else:
+        res = hand_cards_values2(hand)
+    log(f'Evaluated {hand}: {res}')
+    return res
+
 def part1(filename):
     scan = get_lines(filename)
     hands = []
@@ -66,8 +122,13 @@ def part1(filename):
     return sum((i+1)*p[2] for i, p in enumerate(sorted_hands))
 
 def part2(filename):
-    return -1
-
+    scan = get_lines(filename)
+    hands = []
+    for i, line in enumerate(scan):
+        hand, bid = line.split()
+        hands.append((hand, group_hand(hand), int(bid)))
+    sorted_hands = sorted(hands, key=lambda x: compute_hand2(x))
+    return sum((i+1)*p[2] for i, p in enumerate(sorted_hands))
 
 class TestPart1(unittest.TestCase):
     def test_sample(self):
